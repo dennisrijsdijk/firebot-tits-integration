e12148776655b6c5c3a80b7800be35c3314f69b4
Dennis Rijsdijk <git@dennis.gg>
2023-01-14 13:18:36 +0100

    Sample data for EventSource

--- src/events.ts
+++ src/events.ts
@@ -9,20 +9,33 @@ export const TITSEventSource: EventSource = {
             name: "T.I.T.S Hit",
             description: "When an object hits you in T.I.T.S",
             manualMetadata: {
-                sceneName: "Test Scene Name",
+                itemID: "00000000-0000-0000-0000-000000000000",
+                itemName: "Test Item",
+                strength: 24.8588161,
+                x: 0.1441319,
+                y: 0.967980564,
+                z: 0.2055228,
+                magnitude: 1.0,
+                sqrMagnitude: 1.0
             },
         },
         {
             id: "trigger-activated",
             name: "T.I.T.S Trigger Activated",
             description: "When a trigger is activated in T.I.T.S",
-            manualMetadata: {},
+            manualMetadata: {
+                triggerId: "00000000-0000-0000-0000-000000000000",
+                triggerName: "Test Trigger"
+            },
         },
         {
             id: "trigger-ended",
             name: "T.I.T.S Trigger Ended",
             description: "When a trigger has ended in T.I.T.S",
-            manualMetadata: {},
+            manualMetadata: {
+                triggerId: "00000000-0000-0000-0000-000000000000",
+                triggerName: "Test Trigger"
+            },
         },
     ],
 };
